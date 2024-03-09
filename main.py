35407de0394c25219439531c53842e663e4e6ebf
amirZand99 <amirzandimanseh1380@gmail.com>
2024-03-09 20:24:29 +0330

    create gameover

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
 import pygame
+import time
 
 pygame.init()
-screen = pygame.display.set_mode((800, 900))
+dis_width = 800
+dis_height = 900
+
+screen = pygame.display.set_mode((dis_width, dis_height))
 image = pygame.image.load('/home/amir/Downloads/_400b86d6-92a7-40f4-b294-470887be9914.jpeg')
-screen.blit(image, (0, 0))
-pygame.display.update()
 pygame.display.set_caption('Snake game by amir')
 
-game_over=False
+game_over = False
 paused = False
 
 x1 = 400
 y1 = 450
+
+snake_block = 10
+
+font_style = pygame.font.SysFont(None, 90)
+
+
+def message(msg, color):
+    msg = font_style.render(msg, True, color)
+    screen.blit(msg, [dis_width / 2 - 100 , dis_height / 2])
+
 
 x1_change = 0
 y1_change = 0
 
 clock = pygame.time.Clock()
 
-blue=(0, 0, 255)
-white = (255, 255, 255)
+blue = (0, 0, 255)
+red = (255, 0, 0)
 
 while not game_over:
     for event in pygame.event.get():
-        print(event)
         if event.type == pygame.QUIT:
             game_over = True
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                quit()
+                game_over = True
             elif event.key == pygame.K_SPACE:
                 paused = not paused  # Toggle pause with Space key
                 if paused:
@@ -37,27 +47,36 @@ while not game_over:
                     y1_change = 0
 
             elif event.key == pygame.K_LEFT and not paused:
-                x1_change = -10
+                x1_change = -snake_block
                 y1_change = 0
             elif event.key == pygame.K_RIGHT and not paused:
-                x1_change = 10
+                x1_change = snake_block
                 y1_change = 0
             elif event.key == pygame.K_UP and not paused:
-                y1_change = -10
+                y1_change = -snake_block
                 x1_change = 0
             elif event.key == pygame.K_DOWN and not paused:
-                y1_change = 10
+                y1_change = snake_block
                 x1_change = 0
 
     screen.blit(image, (0, 0))
-
     x1 += x1_change
     y1 += y1_change
-    # screen.fill(white)
-    pygame.draw.rect(screen, blue, [x1, y1, 10, 10])
+
+    # Check if snake hits the boundaries
+    if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+        game_over = True
+
+    pygame.draw.rect(screen, blue, [x1, y1, snake_block, snake_block])
     pygame.display.update()
 
     clock.tick(25)
+
+if game_over:
+    screen.blit(image, (0, 0))
+    message("You lost", red)
+    pygame.display.update()
+    time.sleep(2)
 
 pygame.quit()
 quit()
