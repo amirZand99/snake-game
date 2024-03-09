832f96c379388b27a4c011d8e51b19746beb22ae
amirZand99 <amirzandimanseh1380@gmail.com>
2024-03-09 18:36:32 +0330

    create a simple blue snake

--- main.py
+++ main.py
@@ -7,11 +7,17 @@ screen.blit(image, (0, 0))
 pygame.display.update()
 pygame.display.set_caption('Snake game by amir')
 game_over=False
+
+blue=(0,0,255)
+
 while not game_over:
     for event in pygame.event.get():
         print(event)
         if event.type == pygame.QUIT:  # Check if the user clicked the close button
             game_over = True  # Exit the game loop
+
+    pygame.draw.rect(screen, blue, [300, 200, 20, 20])
+    pygame.display.update()
 
 pygame.quit()
 quit()
