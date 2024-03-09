f5d5e44aa8eecce2c339acd3c82ec1b01b7ae30c
amirZand99 <amirzandimanseh1380@gmail.com>
2024-03-09 18:21:06 +0330

    create the screen with image background

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 900))
+image = pygame.image.load('/home/amir/Downloads/_400b86d6-92a7-40f4-b294-470887be9914.jpeg')
+screen.blit(image, (0, 0))
+pygame.display.update()
+pygame.display.set_caption('Snake game by amir')
+game_over=False
+while not game_over:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:  # Check if the user clicked the close button
+            game_over = True  # Exit the game loop
+
+pygame.quit()
+quit()
